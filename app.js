46a8980a8a44badd9744940f35364eb24d5a1ece
Samuel Oduba <samueloduba123@gmail.com>
2022-12-13 11:39:27 +0100

    added procfile

--- app.js
+++ app.js
@@ -18,13 +18,12 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100, //limit each IP to 100 request per windowMs
-  })
-);
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
 // extra packages
 app.use(helmet());
 app.use(cors());
@@ -36,10 +35,6 @@ const connectDB = require("./db/connect");
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
-
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
